dccc5a8fc642ff94711c62e89e7311a7830678ad
Jon Bake <jonmbake@gmail.com>
2018-01-19 20:08:38 -0600

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# checkstyle-import-order-test
---
+++ src/main/java/ImportOrderTest.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+import java.util.List;
+
+/**
+ * Test for CheckStyle's ImportOrderCheck w/ spaces.
+ */
+public class ImportOrderTest {
+}
